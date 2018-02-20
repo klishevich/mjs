7bbe80406629fd58654713f7bd7e9e6aebe21122
Michael Klishevich <busation@gmail.com>
2018-02-20 23:33:03 +0300

    let, const

---
+++ 1_es5_es6/3_es6_const_let.js
@@ -0,0 +1,52 @@
+// ES6
+
+// for (var i=0; i<10; i++) {
+//   console.log(i);
+// }
+// console.log('after loop', i);
+
+
+
+// function scope ES5
+
+// function loop() {
+//   for (var i=0; i<10; i++) {
+//     console.log(i);
+//   }
+// };
+// loop();
+// console.log('after loop', i);
+
+
+
+// let
+// var i = 9999;
+// for (var i=0; i<10; i++) {
+//   console.log(i);
+// }
+// console.log('after loop', i);
+
+
+
+// var i = 9999;
+// if (true) {
+//   let i = 8888;
+// }
+// console.log('after loop', i);
+
+
+
+// const 
+// 'use strict';
+// let a = 1;
+// a = 2;
+// console.log(a);
+
+
+'use strict';
+const person = {
+  name: 'Mike'
+};
+person.name = 'John';
+console.log(person);
+
