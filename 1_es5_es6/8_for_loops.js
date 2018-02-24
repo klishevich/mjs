a609bc8c5b65c3ed507eb47df55c7ddbaa87909e
Michael Klishevich <busation@gmail.com>
2018-02-24 13:13:33 +0300

    fix

--- 1_es5_es6/8_for_loops.js
+++ 1_es5_es6/8_for_loops.js
@@ -1,16 +1,38 @@
-let tvShows = ['Game of Thrones', 'Black Mirror', 'Silicon Valley'];
-tvShows.name = 'Michael Klishevich';
+// For objects
+let tvShowsObj = {
+  first: 'Game of Thrones',
+  second: 'Black Mirror',
+  third: 'Silicon Valley',
+  0: 'Breaking Bad'
+};
+
+// forEach only for Arrays
 // Object.keys(tvShows).forEach(function(key) {
 //   console.log(key, tvShows[key]);
 // })
 
-// for in - works with objects
-// for (let key in tvShows) {
-//   console.log(key, tvShows[key]);
+// for (let key in tvShowsObj) {
+//   console.log(key, tvShowsObj[key]);
 // }
 
-// for of works only with iterable objects (Array, Map, Set, TypedArray, String, arguments)
-// ignore any non-indexed properties
-for(let show of tvShows) {
-  console.log(show);
-}
+// For Arrays
+// let tvShowsArr = ['Game of Thrones', 'Black Mirror', 'Silicon Valley', 'Breaking Bad'];
+// tvShowsArr.hack = 'Someone added this to array WTF';
+// console.log(Object.getOwnPropertyNames(Array.prototype));
+
+// Object.keys(tvShowsArr).forEach(function(key) {
+//   console.log(key, tvShowsArr[key]);
+// })
+
+// for (let key in tvShowsArr) {
+//   console.log(key, tvShowsArr[key]);
+// }
+
+// tvShowsArr.forEach(function(value) {
+//   console.log('value', value);
+// })
+
+// // for of works only with iterable objects (Array, Map, Set, TypedArray, String, arguments)
+// for (let value of tvShowsArr) {
+//   console.log(value);
+// }
