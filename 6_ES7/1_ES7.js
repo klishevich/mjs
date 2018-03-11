942a2447d6ea6d8656074fefbba4dce0706973a9
Michael Klishevich <busation@gmail.com>
2018-03-11 10:19:31 +0300

    uncomment es7 lesson

--- 6_ES7/1_ES7.js
+++ 6_ES7/1_ES7.js
@@ -1,19 +1,19 @@
 // ES7
 // INCLUDES
 
-// const ar = ['a', 'b', 'c'];
-// const check1 = ar.includes('a');
-// console.log(check1);
-// const check2 = ar.includes('x');
-// console.log(check2);
+const ar = ['a', 'b', 'c'];
+const check1 = ar.includes('a');
+console.log(check1);
+const check2 = ar.includes('x');
+console.log(check2);
 
-// const str = 'react';
-// const check1 = str.includes('r');
-// console.log(check1);
-// const check2 = str.includes('x');
-// console.log(check2);
+const str = 'react';
+const check1 = str.includes('r');
+console.log(check1);
+const check2 = str.includes('x');
+console.log(check2);
 
 
 // POWER
-// const res = 10 ** 3;
-// console.log(res);
+const res = 10 ** 3;
+console.log(res);
--- 6_ES7/3_async_func_examples.js
+++ 6_ES7/3_async_func_examples.js
@@ -4,92 +4,92 @@
 
 // 1 Promisify setTimeout
 
-// setTimeout(() => { console.log('trololo') }, 1000);
+setTimeout(() => { console.log('trololo') }, 1000);
 
-// function setTimeoutP(func, time) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       try {
-//         const res = func();
-//         resolve(res);
-//       }
-//       catch(error) {
-//         reject(error);
-//       }
-//     }, time);
-//   });
-// }
+function setTimeoutP(func, time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        const res = func();
+        resolve(res);
+      }
+      catch(error) {
+        reject(error);
+      }
+    }, time);
+  });
+}
 
-// setTimeoutP(() => { console.log('trololo') }, 1000);
+setTimeoutP(() => { console.log('trololo') }, 1000);
 
-// setTimeoutP(() => 'trololo', 1000)
-// .then(res => console.log(res))
+setTimeoutP(() => 'trololo', 1000)
+.then(res => console.log(res))
 
 // EXAMPLES
 
-// const setTimeoutP = require('./exportSetTimeoutP.js');
+const setTimeoutP = require('./exportSetTimeoutP.js');
 
-// // Async function declarations
+// Async function declarations
 
-// async function foo() {
-//   const res = await setTimeoutP(() => 'trololo2', 1000);
-//   console.log(res);
-//   return res;
-// }
-// foo()
+async function foo() {
+  const res = await setTimeoutP(() => 'trololo2', 1000);
+  console.log(res);
+  return res;
+}
+foo()
 
-// // Async function expressions
-// const foo = async function () {
-//   const res = await setTimeoutP(() => 'trololo3', 1000);
-//   console.log(res);
-//   return res;
-// }
-// foo()
+// Async function expressions
+const foo = async function () {
+  const res = await setTimeoutP(() => 'trololo3', 1000);
+  console.log(res);
+  return res;
+}
+foo()
 
-// // Async method definitions
-// let obj = {
-//   async foo() {
-//     const res = await setTimeoutP(() => 'trololo4', 1000);
-//     console.log(res);
-//     return res;
-//   }
-// }
-// obj.foo()
+// Async method definitions
+let obj = {
+  async foo() {
+    const res = await setTimeoutP(() => 'trololo4', 1000);
+    console.log(res);
+    return res;
+  }
+}
+obj.foo()
 
-// // Async arrow function
-// const foo = async () => {
-//   const res = await setTimeoutP(() => 'trololo3', 1000);
-//   console.log(res);
-//   return res;
-// }
-// foo()
+// Async arrow function
+const foo = async () => {
+  const res = await setTimeoutP(() => 'trololo3', 1000);
+  console.log(res);
+  return res;
+}
+foo()
 
 
 // ASYNC FUNCTIONS ALWAYS RETURN PROMISES
 
-// const setTimeoutP = require('./exportSetTimeoutP.js');
-// async function foo() {
-//   const res = await setTimeoutP(() => 'trololo2', 1000);
-//   console.log(res);
-//   return res;
-// }
-// foo()
-// .then(res => console.log(`Res Again: ${res}`));
+const setTimeoutP = require('./exportSetTimeoutP.js');
+async function foo() {
+  const res = await setTimeoutP(() => 'trololo2', 1000);
+  console.log(res);
+  return res;
+}
+foo()
+.then(res => console.log(`Res Again: ${res}`));
 
 
-// async function asyncFunc() {
-//     return 123;
-// }
-// asyncFunc()
-// .then(x => console.log(x));
+async function asyncFunc() {
+    return 123;
+}
+asyncFunc()
+.then(x => console.log(x));
 
 
-// // Reject promise
-// async function asyncFunc() {
-//     throw new Error('Problem!');
-// }
+// Reject promise
+async function asyncFunc() {
+    throw new Error('Problem!');
+}
 
-// asyncFunc()
-// .catch(err => console.log(err));
-//     // Error: Problem!
+asyncFunc()
+.catch(err => console.log(err));
+    // Error: Problem!
 
